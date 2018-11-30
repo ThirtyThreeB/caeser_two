1e7501b791b8b15aff7bdb5963ca6ef8c9f4cb9d
Gabriel Aragon <thirtythreebananas@gmail.com>
2018-11-30 13:44:39 -0500

    works

--- caeser_two.rb
+++ caeser_two.rb
@@ -1,10 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/' do
-	"Caeser Two"
+
+def caeser(str, n)
+  arr = str.split("")
+  newArr = []
+
+  arr.each do |x|
+  # newArr << x.ord + n.to_i
+	  if (x.ord + n.to_i).between?(65,90) | (x.ord + n.to_i).between?(97,122)
+	    newArr << (x.ord+n.to_i).chr
+	  elsif (x.ord == 32) | (x.ord == 33)
+	    newArr << x.ord.chr
+	  else
+	    newArr << (x.ord + n.to_i-26).chr
+	  end
+  end
+   newArr.join
 end
 
-get '/home' do
-	"This is the home page evidently"
+get '/' do
+
+	str = params[:phrase]
+	n = params[:num]
+	cipher_str = caeser(str, n)
+
+	erb :index, :locals => {:cipher_str => cipher_str}
+	
 end
+
+
+
